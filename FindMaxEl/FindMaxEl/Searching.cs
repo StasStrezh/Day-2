fc19aaef3a3546d721c249e11b9fea6058c4a28c
Stas Strezh <37235564+StasStrezh@users.noreply.github.com>
2018-03-15 20:51:57 +0300

    Add files via upload

---
+++ FindMaxEl/FindMaxEl/Searching.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxEl
+{
+    public class Searching
+    {
+        public static int Search(int[] array)
+        {
+            return Maximum(array, 0, array.Length - 1);
+        }
+        static int Maximum(int[] arr, int left, int right)
+        {
+            if (left == right)
+                return arr[left];
+            else
+            {
+                int buff1 = Maximum(arr, left, (left + right) / 2);
+                int buff2 = Maximum(arr, (left + right) / 2 + 1, right);
+
+                return Math.Max(buff1, buff2);
+            }
+        }
+    }
+}
